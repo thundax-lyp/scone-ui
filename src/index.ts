c3a449187a9862871ba4046c942543bf45e8f73d
Thundax <thundax@qq.com>
2026-07-14 17:18:12 +0800

    Refactor(components): 整理公共入口导出分组

--- src/index.ts
+++ src/index.ts
@@ -34,48 +34,6 @@ export type {
     SconeTypographyTone,
     SconeTypographyWeight,
 } from "./components/data-display";
-export {
-    SconeAccordion,
-    SconeBreadcrumb,
-    SconeCollapsible,
-    SconeCommand,
-    SconeDropdown,
-    SconeDropdownItem,
-    SconeDropdownLabel,
-    SconeDropdownSeparator,
-    SconeMenu,
-    SconeSegmented,
-    SconeTabs,
-    SconeTooltip,
-    SconeTree,
-} from "./components/navigation";
-export type {
-    SconeAccordionItem,
-    SconeAccordionProps,
-    SconeActionItem,
-    SconeBreadcrumbItem,
-    SconeBreadcrumbProps,
-    SconeCollapsibleProps,
-    SconeCommandItem,
-    SconeCommandProps,
-    SconeDropdownProps,
-    SconeMenuProps,
-    SconeNavigationItem,
-    SconeSegmentedProps,
-    SconeTabsContentProps,
-    SconeTabsItem,
-    SconeTabsListProps,
-    SconeTabsProps,
-    SconeTabsTriggerProps,
-    SconeTooltipProps,
-    SconeTreeCheckInfo,
-    SconeTreeExpandInfo,
-    SconeTreeNode,
-    SconeTreeProps,
-    SconeTreeSelectInfo,
-} from "./components/navigation";
-export { SconeAvatar, SconeImage } from "./components/media";
-export type { SconeAvatarProps, SconeImageProps } from "./components/media";
 
 export {
     SconeButton,
@@ -129,38 +87,37 @@ export type {
     SconeUploadRejection,
 } from "./components/form";
 
-export { ariaBoolean, ariaValue, hasAriaValue, mergeAriaDescribedBy, mergeIds } from "./lib/aria";
-export { SconePagination } from "./components/navigation";
+export { SconeCompact } from "./components/layout/compact";
+export type { SconeCompactProps } from "./components/layout/compact";
+export { SconeInline } from "./components/layout/inline";
+export type { SconeInlineProps } from "./components/layout/inline";
+export { SconeScrollArea } from "./components/layout/scroll-area";
+export type { SconeScrollAreaProps } from "./components/layout/scroll-area";
+export { SconeSeparator } from "./components/layout/separator";
+export type { SconeSeparatorProps } from "./components/layout/separator";
+export { SconeSplitPane } from "./components/layout/split-pane";
+export type { SconeSplitPaneProps, SconeSplitPaneSizePreset } from "./components/layout/split-pane";
+export { SconeStack } from "./components/layout/stack";
+export type { SconeStackProps } from "./components/layout/stack";
+export { SconeToolbar } from "./components/layout/toolbar";
+export type { SconeToolbarProps } from "./components/layout/toolbar";
+
 export { SconeAlert } from "./components/feedback-overlay/alert";
+export type { SconeAlertProps } from "./components/feedback-overlay/alert";
 export { SconeConfirm } from "./components/feedback-overlay/confirm";
+export type { SconeConfirmProps } from "./components/feedback-overlay/confirm";
 export { SconeDialog } from "./components/feedback-overlay/dialog";
+export type { SconeDialogProps } from "./components/feedback-overlay/dialog";
 export { SconeDrawer } from "./components/feedback-overlay/drawer";
+export type { SconeDrawerProps } from "./components/feedback-overlay/drawer";
 export { SconeEmpty } from "./components/feedback-overlay/empty";
+export type { SconeEmptyProps } from "./components/feedback-overlay/empty";
 export { SconeLoading } from "./components/feedback-overlay/loading";
+export type { SconeLoadingProps } from "./components/feedback-overlay/loading";
 export {
     SconeNotificationProvider,
     notification,
 } from "./components/feedback-overlay/notification";
-export { SconeProgress } from "./components/feedback-overlay/progress";
-export { SconeToastProvider, toast } from "./components/feedback-overlay/toast";
-export { AppShell, DataTable, FilterBar, Page, Section } from "./patterns";
-export { SconeCompact } from "./components/layout/compact";
-export { SconeInline } from "./components/layout/inline";
-export { SconeScrollArea } from "./components/layout/scroll-area";
-export { SconeSeparator } from "./components/layout/separator";
-export { SconeSplitPane } from "./components/layout/split-pane";
-export { SconeStack } from "./components/layout/stack";
-export { SconeToolbar } from "./components/layout/toolbar";
-export { cn } from "./lib/cn";
-export { composeRefs } from "./lib/compose-refs";
-export { useControllableState } from "./lib/use-controllable-state";
-
-export type { SconeAlertProps } from "./components/feedback-overlay/alert";
-export type { SconeConfirmProps } from "./components/feedback-overlay/confirm";
-export type { SconeDialogProps } from "./components/feedback-overlay/dialog";
-export type { SconeDrawerProps } from "./components/feedback-overlay/drawer";
-export type { SconeEmptyProps } from "./components/feedback-overlay/empty";
-export type { SconeLoadingProps } from "./components/feedback-overlay/loading";
 export type {
     NotificationCloseReason,
     NotificationOptions,
@@ -169,7 +126,9 @@ export type {
     SconeNotificationItem,
     SconeNotificationProviderProps,
 } from "./components/feedback-overlay/notification";
+export { SconeProgress } from "./components/feedback-overlay/progress";
 export type { SconeProgressProps } from "./components/feedback-overlay/progress";
+export { SconeToastProvider, toast } from "./components/feedback-overlay/toast";
 export type {
     SconeToastItem,
     SconeToastProviderProps,
@@ -178,14 +137,54 @@ export type {
     ToastPosition,
     ToastService,
 } from "./components/feedback-overlay/toast";
-export type { SconeCompactProps } from "./components/layout/compact";
-export type { SconeInlineProps } from "./components/layout/inline";
-export type { SconeScrollAreaProps } from "./components/layout/scroll-area";
-export type { SconeSeparatorProps } from "./components/layout/separator";
-export type { SconeSplitPaneProps, SconeSplitPaneSizePreset } from "./components/layout/split-pane";
-export type { SconeStackProps } from "./components/layout/stack";
-export type { SconeToolbarProps } from "./components/layout/toolbar";
-export type { SconePaginationProps } from "./components/navigation";
+
+export { SconeAvatar, SconeImage } from "./components/media";
+export type { SconeAvatarProps, SconeImageProps } from "./components/media";
+
+export {
+    SconeAccordion,
+    SconeBreadcrumb,
+    SconeCollapsible,
+    SconeCommand,
+    SconeDropdown,
+    SconeDropdownItem,
+    SconeDropdownLabel,
+    SconeDropdownSeparator,
+    SconeMenu,
+    SconePagination,
+    SconeSegmented,
+    SconeTabs,
+    SconeTooltip,
+    SconeTree,
+} from "./components/navigation";
+export type {
+    SconeAccordionItem,
+    SconeAccordionProps,
+    SconeActionItem,
+    SconeBreadcrumbItem,
+    SconeBreadcrumbProps,
+    SconeCollapsibleProps,
+    SconeCommandItem,
+    SconeCommandProps,
+    SconeDropdownProps,
+    SconeMenuProps,
+    SconeNavigationItem,
+    SconePaginationProps,
+    SconeSegmentedProps,
+    SconeTabsContentProps,
+    SconeTabsItem,
+    SconeTabsListProps,
+    SconeTabsProps,
+    SconeTabsTriggerProps,
+    SconeTooltipProps,
+    SconeTreeCheckInfo,
+    SconeTreeExpandInfo,
+    SconeTreeNode,
+    SconeTreeProps,
+    SconeTreeSelectInfo,
+} from "./components/navigation";
+
+export { AppShell, DataTable, FilterBar, Page, Section } from "./patterns";
 export type {
     AppShellAsideProps,
     AppShellHeaderProps,
@@ -219,6 +218,11 @@ export type {
     SectionRootProps,
     SectionTitleProps,
 } from "./patterns";
+
+export { ariaBoolean, ariaValue, hasAriaValue, mergeAriaDescribedBy, mergeIds } from "./lib/aria";
+export { cn } from "./lib/cn";
+export { composeRefs } from "./lib/compose-refs";
+export { useControllableState } from "./lib/use-controllable-state";
 
 export type {
     Breakpoint,
